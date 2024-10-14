ba508e9b1d1638f2247725b4e310e44816968cc1
Lei Chen <Chenleicn99@gmail.com>
2024-10-14 14:43:58 +0200

    test(auth): update the test to see if it passes CI check

--- app/src/androidTest/java/com/android/brewr/ui/authentication/SignInTest.kt
+++ app/src/androidTest/java/com/android/brewr/ui/authentication/SignInTest.kt
@@ -2,12 +2,14 @@ package com.android.brewr.ui.authentication
 
 import androidx.compose.ui.test.assertHasClickAction
 import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithContentDescription
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.android.brewr.MainActivity
 import com.android.brewr.ui.navigation.NavigationActions
 import org.junit.*
 import org.junit.runner.RunWith
@@ -16,7 +18,7 @@ import org.mockito.Mockito.*
 @RunWith(AndroidJUnit4::class)
 class SignInScreenTest {
 
-  @get:Rule val composeTestRule = createComposeRule()
+  @get:Rule val composeTestRule = createAndroidComposeRule<MainActivity>()
 
   private lateinit var navigationActions: NavigationActions
 
@@ -27,20 +29,18 @@ class SignInScreenTest {
 
   @Test
   fun signInScreen_displaysCorrectly() {
-    composeTestRule.setContent { SignInScreen(navigationActions) }
     composeTestRule.onNodeWithContentDescription("App Logo").assertIsDisplayed()
     composeTestRule.onNodeWithTag("loginTitle").assertIsDisplayed()
     composeTestRule.onNodeWithText("BrewR").assertIsDisplayed()
     composeTestRule.onNodeWithTag("loginButton").assertIsDisplayed()
     composeTestRule.onNodeWithText("Sign in with Google").assertIsDisplayed()
     composeTestRule.onNodeWithContentDescription("Google Logo").assertIsDisplayed()
-    composeTestRule.onNodeWithTag("loginButton").assertHasClickAction()
   }
 
   @Test
   fun testLoginButtonStartsSignInProcess() {
-    composeTestRule.setContent { SignInScreen(navigationActions) }
     // Click the login button
+    composeTestRule.onNodeWithTag("loginButton").assertHasClickAction()
     composeTestRule.onNodeWithTag("loginButton").performClick()
   }
 }
