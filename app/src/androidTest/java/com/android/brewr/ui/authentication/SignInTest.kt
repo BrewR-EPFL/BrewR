cca8d9c79dfec8e16284e4fefce1f7a29e9b7546
Lei Chen <Chenleicn99@gmail.com>
2024-10-14 15:35:13 +0200

    feat(auth): modify the display of Toast message using withContext(Dispatchers.Main)

--- app/src/androidTest/java/com/android/brewr/ui/authentication/SignInTest.kt
+++ app/src/androidTest/java/com/android/brewr/ui/authentication/SignInTest.kt
@@ -3,7 +3,6 @@ package com.android.brewr.ui.authentication
 import androidx.compose.ui.test.assertHasClickAction
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
-import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithContentDescription
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.onNodeWithText
