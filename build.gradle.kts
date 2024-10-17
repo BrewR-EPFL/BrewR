3276e00f7d087f4c577d9a336e8ace03b6e3ea4d
ngirod <nathanael.girod@epfl.ch>
2024-10-17 15:57:44 +0200

    chore: change to Kotlin 2.0 and install coilCompose dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,8 @@
 plugins {
     alias(libs.plugins.androidApplication) apply false
     alias(libs.plugins.jetbrainsKotlinAndroid) apply false
+    alias(libs.plugins.compose.compiler) apply false
+
 }
 
 buildscript {
