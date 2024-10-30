acc434c840a354b9882d0aa926a173283d892b86
Jaeyi Jeong <jaeyi.jeong@epgl.ch>
2024-10-30 23:01:20 +0100

    fix: add name parameter in location

--- app/src/main/java/com/android/brewr/model/journey/Location.kt
+++ app/src/main/java/com/android/brewr/model/journey/Location.kt
@@ -1,6 +1,3 @@
 package com.android.brewr.model.journey
 
-data class Location(
-    val latitude: Double,
-    val longitude: Double
-)
+data class Location(val latitude: Double, val longitude: Double, val name: String)
