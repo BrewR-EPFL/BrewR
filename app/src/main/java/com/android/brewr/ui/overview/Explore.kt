7c2c475f69dd1d87929e7c40d59854a32c97c303
Jaeyi Jeong <jaeyi.jeong@epgl.ch>
2024-10-30 23:00:30 +0100

    feat: integrate MapScreen into ExploreScreen

--- app/src/main/java/com/android/brewr/ui/overview/Explore.kt
+++ app/src/main/java/com/android/brewr/ui/overview/Explore.kt
@@ -2,17 +2,15 @@ package com.android.brewr.ui.overview
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.testTag
+import com.android.brewr.model.journey.Location
 
 @Composable
-fun ExploreScreen() {
+fun ExploreScreen(listLocations: List<Location>) {
   Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-    Text(
-        modifier = Modifier.testTag("exploreContent"),
-        text = "Explore content here, not yet implemented.")
+    // Call MapScreen within ExploreScreen, passing locations
+    MapScreen(listLocations = listLocations)
   }
 }
