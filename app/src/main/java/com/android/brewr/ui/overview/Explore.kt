2779966f6e91a2a6c864fdb61b17d27ed13197cc
Jaeyi Jeong <jaeyi.jeong@epgl.ch>
2024-11-01 17:13:39 +0100

    fix: revert to the original explore screen code

--- app/src/main/java/com/android/brewr/ui/overview/Explore.kt
+++ app/src/main/java/com/android/brewr/ui/overview/Explore.kt
@@ -2,15 +2,17 @@ package com.android.brewr.ui.overview
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import com.android.brewr.model.journey.Location
+import androidx.compose.ui.platform.testTag
 
 @Composable
-fun ExploreScreen(listLocations: List<Location>) {
+fun ExploreScreen() {
   Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-    // Call MapScreen within ExploreScreen, passing locations
-    MapScreen(listLocations = listLocations)
+    Text(
+        modifier = Modifier.testTag("exploreContent"),
+        text = "Explore content here, not yet implemented.")
   }
 }
