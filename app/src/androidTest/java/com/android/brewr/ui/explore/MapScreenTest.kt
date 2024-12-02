0250795b1dba9544402021a090fb3518111bf27d
Pablo Robin Guerrero <104727746+veridisyndrome@users.noreply.github.com.>
2024-12-02 11:42:37 +0100

    fix: MapScreenTest.kt Location import

--- app/src/androidTest/java/com/android/brewr/ui/explore/MapScreenTest.kt
+++ app/src/androidTest/java/com/android/brewr/ui/explore/MapScreenTest.kt
@@ -10,12 +10,21 @@ import androidx.test.uiautomator.UiSelector
 import com.android.brewr.model.coffee.Coffee
 import com.android.brewr.model.coffee.Hours
 import com.android.brewr.model.coffee.Review
-import com.android.brewr.model.location.Location
+import com.android.brewr.model.journey.BrewingMethod
+import com.android.brewr.model.journey.CoffeeOrigin
+import com.android.brewr.model.journey.CoffeeRate
+import com.android.brewr.model.journey.CoffeeTaste
+import com.android.brewr.model.journey.Journey
+import com.android.brewr.model.journey.JourneysRepository
+import com.android.brewr.model.journey.ListJourneysViewModel
+import com.android.brewr.model.map.Location
+import com.google.firebase.Timestamp
 import kotlinx.coroutines.runBlocking
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.mockito.Mockito.mock
 
 @RunWith(AndroidJUnit4::class)
 class MapScreenTest {
@@ -23,49 +32,75 @@ class MapScreenTest {
   @get:Rule val composeTestRule = createComposeRule()
 
   private lateinit var uiDevice: UiDevice
+  private lateinit var listJourneysViewModel: ListJourneysViewModel
 
   @Before
   fun setUp() {
     // Initialize UiDevice instance
     uiDevice = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation())
+    listJourneysViewModel = ListJourneysViewModel(mock(JourneysRepository::class.java))
 
     // Sample list of locations for testing
+    val sampleJourney =
+        Journey(
+            "1",
+            "",
+            "",
+            Location(
+                latitude = 46.5228,
+                longitude = 6.6285,
+                name =
+                    "Shoreline Golf Links, 2940, North Shoreline Boulevard, Mountain View, Santa Clara County, California, 94043, United States"),
+            CoffeeOrigin.DEFAULT,
+            BrewingMethod.DEFAULT,
+            CoffeeTaste.DEFAULT,
+            CoffeeRate.DEFAULT,
+            Timestamp.now())
+    listJourneysViewModel.addJourney(sampleJourney)
     val sampleCoffees =
         listOf(
             Coffee(
                 "1",
                 "Starbucks",
-                Location(latitude = 46.5228, longitude = 6.6285, address = "Lausanne 1"),
+                Location(latitude = 46.5228, longitude = 6.6285, name = "Lausanne 1"),
                 4.5,
                 listOf(Hours("Monday", "10", "20"), Hours("Tuesday", "10", "20")),
                 listOf(Review("Lei", "good", 5.0)),
                 listOf("test.jpg")),
             Coffee(
                 "2",
+                "Shoreline Golf Links,",
+                Location(latitude = 37.4305087, longitude = -122.0854755, name = "Lausanne 1"),
+                4.5,
+                listOf(Hours("Monday", "10", "20"), Hours("Tuesday", "10", "20")),
+                listOf(Review("Pablo", "Awesome", 5.0)),
+                listOf("test.jpg")),
+            Coffee(
+                "3",
                 "McDonald's",
-                Location(latitude = 46.5228, longitude = 6.7285, address = "Lausanne 1"),
+                Location(latitude = 46.5228, longitude = 6.7285, name = "Lausanne 1"),
                 4.5,
                 listOf(Hours("Monday", "10", "20"), Hours("Tuesday", "10", "20")),
                 listOf(Review("Lei", "good", 5.0)),
                 listOf("test.jpg")),
             Coffee(
-                "3",
+                "4",
                 "default",
-                Location(latitude = 46.5228, longitude = 6.7285, address = "Lausanne 1"),
+                Location(latitude = 46.5228, longitude = 6.7285, name = "Lausanne 1"),
                 4.5,
                 listOf(Hours("Monday", "10", "20"), Hours("Tuesday", "10", "20")),
                 listOf(Review("Lei", "good", 5.0)),
                 listOf("test.jpg")))
 
     // Set content to ExploreScreen
-    composeTestRule.setContent { MapScreen(sampleCoffees) }
+    composeTestRule.setContent { MapScreen(sampleCoffees, listJourneysViewModel) }
 
     // Handle location permission if prompted
     runBlocking { grantLocationPermission() }
   }
 
   @Test
-  fun mapIsDisplayedInExploreScreen() {
+  fun mapScreen_IsDisplayedInExploreScreen() {
     // Verify that the map (tagged as "mapScreen") is displayed
     composeTestRule.onNodeWithTag("mapScreen").assertIsDisplayed()
   }
