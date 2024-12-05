910691fcd75631c4768ca06c035ab4ba51f55d6b
Lei Chen <Chenleicn99@gmail.com>
2024-12-05 18:56:56 +0100

    test: add tests

---
+++ app/src/androidTest/java/com/android/brewr/ui/UserProfile/UserPrivateListScreenTest.kt
@@ -0,0 +1,32 @@
+package com.android.brewr.ui.userProfile
+
+import androidx.compose.ui.test.*
+import androidx.compose.ui.test.junit4.createComposeRule
+import com.android.brewr.ui.navigation.NavigationActions
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.mockito.Mockito.mock
+
+class UserPrivateListScreenTest {
+
+  @get:Rule val composeTestRule = createComposeRule()
+  private lateinit var navigationActions: NavigationActions
+
+  @Before
+  fun setUp() {
+    // Mock NavController
+    navigationActions = mock(NavigationActions::class.java)
+  }
+
+  @Test
+  fun userPrivateListScreen_displaysComponents() {
+    composeTestRule.setContent { UserPrivateListScreen(navigationActions) }
+
+    composeTestRule.onNodeWithTag("UserPrivateListScreen").assertIsDisplayed()
+    composeTestRule.onNodeWithTag("topBar").assertIsDisplayed()
+    composeTestRule.onNodeWithTag("privateList").assertIsDisplayed()
+    composeTestRule.onNodeWithTag("coffeeImage:1").performClick()
+    composeTestRule.onNodeWithTag("goBackButton").assertIsDisplayed().performClick()
+  }
+}
