c3bd48a7e36cf7a1504bf6e08833b585ae2adc91
Lei Chen <Chenleicn99@gmail.com>
2024-12-05 20:54:36 +0100

    test: fix bugs in tests

--- app/src/androidTest/java/com/android/brewr/ui/UserProfile/UserPrivateListScreenTest.kt
+++ app/src/androidTest/java/com/android/brewr/ui/UserProfile/UserPrivateListScreenTest.kt
@@ -7,7 +7,7 @@ import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.mockito.Mockito.mock
-import org.mockito.kotlin.verify
+import org.mockito.Mockito.verify
 
 class UserPrivateListScreenTest {
 
@@ -23,12 +23,17 @@ class UserPrivateListScreenTest {
   @Test
   fun userPrivateListScreen_displaysComponents() {
     composeTestRule.setContent { UserPrivateListScreen(navigationActions) }
-
     composeTestRule.onNodeWithTag("UserPrivateListScreen").assertIsDisplayed()
     composeTestRule.onNodeWithTag("topBar").assertIsDisplayed()
     composeTestRule.onNodeWithTag("privateList").assertIsDisplayed()
+    composeTestRule.onNodeWithTag("goBackButton").assertIsDisplayed()
     composeTestRule.onNodeWithTag("coffeeImage:1").performClick()
-    composeTestRule.onNodeWithTag("goBackButton").assertIsDisplayed().performClick()
+  }
+
+  @Test
+  fun userPrivateListScreen_goBackButton() {
+    composeTestRule.setContent { UserPrivateListScreen(navigationActions) }
+    composeTestRule.onNodeWithTag("goBackButton").performClick()
     verify(navigationActions).goBack()
   }
 }
