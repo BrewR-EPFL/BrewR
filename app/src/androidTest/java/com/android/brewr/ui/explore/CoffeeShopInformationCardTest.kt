75691e0bb705ce0e32b3145e0d4cb5b5eaf2cc5f
Pablo Robin Guerrero <104727746+veridisyndrome@users.noreply.github.com.>
2024-12-19 16:33:45 +0100

    test: fix display bug

--- app/src/androidTest/java/com/android/brewr/ui/explore/CoffeeShopInformationCardTest.kt
+++ app/src/androidTest/java/com/android/brewr/ui/explore/CoffeeShopInformationCardTest.kt
@@ -4,6 +4,7 @@ import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.assertTextEquals
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.performScrollTo
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.uiautomator.UiDevice
@@ -81,7 +82,9 @@ class CoffeeShopInformationCardTest {
         .assertIsDisplayed()
         .assertTextEquals("Address: " + mockCoffeeShop.location.name)
     composeTestRule
-        .onNodeWithTag("coffeeShopHours:${mockCoffeeShop.id}")
+        .onNodeWithTag(
+            "coffeeShopHour${LocalDate.now().dayOfWeek.name.lowercase().replaceFirstChar { it.uppercase() }}")
+        .performScrollTo()
         .assertIsDisplayed()
         .assertTextEquals(
             "Opening Hours: " +
